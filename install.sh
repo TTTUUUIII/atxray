64ffc12c347404ba4217941b6db2c955f2fc0a87
deliu <de.liu@touchair.cn>
2024-09-26 17:52:09 +0800

    some optimizations for install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,16 @@
 
 ATXRAY_HOME=${ATXRAY_HOME:-$HOME/.atxray}
 
+if [ -d $ATXRAY_HOME ]; then
+	echo -e "\e[31m$ATXRAY_HOME already exists, aborted!\e[0m"
+	exit 1
+fi
+
 if ! git --version >/dev/null 2>&1; then
 	read -p "Continue after installing git? [Y/n]" agreed
 	agreed=$(echo ${argeed:-Y} | tr A-Z a-z)
 	if [ $agreed != "y" ] && [ $agreed != "yes" ]; then
-		echo -e "\e[31mInstall aborted!"
+		echo -e "\e[31mGit not install, aborted!\e[0m"
 		exit 1
 	fi
 	apt install -y git
